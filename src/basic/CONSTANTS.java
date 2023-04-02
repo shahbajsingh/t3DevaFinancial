b5c138c18d5358204faef03e959a039771a5b3bb
Shahbaj <shahbajs987@gmail.com>
2023-04-02 15:31:37 -0400

    Update CONSTANTS.java

--- src/basic/CONSTANTS.java
+++ src/basic/CONSTANTS.java
@@ -4,9 +4,9 @@ import java.sql.Timestamp;
 
 public final class CONSTANTS {
 
-    public static final String DATABASE_URL = "jdbc:mysql://127.0.0.1:3306/?user=root";
+    public static final String DATABASE_URL = ""; // redact
     public static final String HOSTNAME = "root";
-    public static final String PASSWORD = "foobar88";
+    public static final String PASSWORD = ""; // redact
     public static final Timestamp CURRENT_TIME = new Timestamp(System.currentTimeMillis());
     public static final Timestamp ONE_MONTH_FROM_NOW = new Timestamp(System.currentTimeMillis() + 2592000000L);
     public static final Timestamp THREE_MONTHS_FROM_NOW = new Timestamp(System.currentTimeMillis() + 7776000000L);
